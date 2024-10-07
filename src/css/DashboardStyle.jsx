ca25f44007ba1bd46dd2e98efff2a0a5c9b466ee
glaukko <talonelygod@gmail.com>
2024-10-06 23:15:45 -0300

    Fazendo dashboard de exemplo

--- src/css/DashboardStyle.jsx
+++ src/css/DashboardStyle.jsx
@@ -5,19 +5,19 @@ export const DashboardStyle = styled.section
 .dashboard-container {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
     text-align: center;
     padding: 20px;
     border-radius: 10px;
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
-    max-width: 600px;
     margin: 0 auto;
+    height: 100vh;
 }
 
 h1 {
     font-size: 2.5rem;
-    color: white
+    color: white;
     margin-bottom: 10px;
 }
 
--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -8,8 +8,17 @@ const Dashboard = () => {
     return (
         <DashboardStyle>
         <div className="dashboard-container">
-            <h1>Bem vindo, {username}!</h1>
-            <p>Você fez o login com sucesso.</p>
+            <div className="welcome">
+                <h1>Bem vindo, {username}!</h1>
+                <p>Você fez o login com sucesso.</p>
+            </div>
+
+            <div className="watch">
+            <h1>Assista à corrida e concorra a prêmios!</h1>
+            <iframe width="1080" height="720"
+            src="https://www.youtube.com/embed/KezLkVHiXV0">
+            </iframe> 
+            </div>
         </div>
         </DashboardStyle>
     );
